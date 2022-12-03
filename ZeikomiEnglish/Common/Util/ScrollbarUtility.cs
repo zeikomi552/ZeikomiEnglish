9ee40ebe6af53394b731d2b247a793ca664737fc
zeikomi552 <premium.tsubu@gmail.com>
2022-12-04 06:42:34 +0900

    コメント追加

--- ZeikomiEnglish/Common/Util/ScrollbarUtility.cs
+++ ZeikomiEnglish/Common/Util/ScrollbarUtility.cs
@@ -9,11 +9,18 @@ namespace ZeikomiEnglish.Common.Util
 {
     public class ScrollbarUtility
     {
+        #region スクロールバー制御
+        /// <summary>
+        /// スクロールバー制御
+        /// 選択している行にスクロールバーを移動する
+        /// </summary>
+        /// <param name="dg">DataGrid</param>
         public static void TopRow(DataGrid dg)
         {
             dg.ScrollIntoView(dg.Items[dg.SelectedIndex]); // 選択行にスクロールが移動
             dg.UpdateLayout();
 
         }
+        #endregion
     }
 }
