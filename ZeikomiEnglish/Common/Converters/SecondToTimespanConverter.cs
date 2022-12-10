59a19531145477898b5b63a88bd88d586f2a159b
zeikomi552 <premium.tsubu@gmail.com>
2022-12-11 08:05:16 +0900

    経過時間の表示を見やすく変更

---
+++ ZeikomiEnglish/Common/Converters/SecondToTimespanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeikomiEnglish.Common.Converters
+{
+    [System.Windows.Data.ValueConversion(typeof(double), typeof(TimeSpan))]
+    public class SecondToTimespanConverter : System.Windows.Data.IValueConverter
+    {
+
+        #region IValueConverter メンバ
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var target = (double)value;
+            // ここに処理を記述する
+            return new TimeSpan(0,0, (int)target);
+        }
+
+        // TwoWayの場合に使用する
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+
+}
